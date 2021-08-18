11dfc30f73c447403ab48ef7560d92e68d2c55bd
Mihir Mahajan <mihir.mahajan2002@gmail.com>
2021-08-18 21:58:37 +0000

    deployed

--- flask/app/main.py
+++ flask/app/main.py
@@ -1,29 +1,72 @@
-from recommender import recommend
-import numpy as np
-from flask import Flask, request, render_template
-from flask import jsonify
-from flask_cors import CORS
+from flask import Flask, request, render_template, jsonify
+import math
+from openpyxl import load_workbook
+
+wb = load_workbook(filename='/app/app/Toughest Sport by Skill.xlsx')
+sheet_ranges = wb['Sheet1']
+max_dist = math.sqrt(1200)
 
 app = Flask(__name__)
-cors = CORS(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         dict = request.form.to_dict()
-        data = list(dict.items())
-        array = np.array(data)
-        arr = [0 for x in range(10)]
-        for i in range(len(array)):
-            arr[i] = array[i][1]
-        x = np.array(arr)
-        y = x.astype(np.float)
-        outputarr = recommend(y)
-        return jsonify(outputarr)
+        data = list(dict.values())
+        output = recommend(data)
+        return jsonify(output)
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.secret_key = 'super secret key'
-    app.run()
+
+# functions 
+
+def getValue(cell):
+    return sheet_ranges[cell].value
+
+
+def iter_rows():
+    for row in sheet_ranges.iter_rows():
+        yield [cell.value for cell in row]
+
+
+def get_row(row):
+    return [cell.value for cell in sheet_ranges[row]]
+
+
+def get_dist(input, datapoint):
+    if(len(input) != len(datapoint)-3):
+        return RuntimeError("Wrong array length!")
+    power = 0
+    for i in range(1, len(input)-2):
+        power += ((float(input[i]) - datapoint[i]) ** 2)
+    return math.sqrt(power)
+
+
+def get_match(input, datapoint):
+    dist = get_dist(input, datapoint)
+    dist = dist/max_dist
+    dist = dist * 100
+    return 100 - dist
+
+
+def bubble_sort(arr):
+    for i in arr:
+        for j in range(len(arr) - 1):
+            if(arr[j][1] < arr[j+1][1]):
+                tmp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tmp
+    return arr
+
+
+def recommend(input):
+    output = [[0 for x in range(2)] for y in iter_rows()]
+    for row in range(0, len(output)-2):
+        output[row][0] = get_row('A')[row+1]
+        output[row][1] = get_match(input, get_row(row+2))
+    output = bubble_sort(output)
+    return output
+
+
+#print(recommend(["1","1","1","1","1","1","1","1","1","1"]))
